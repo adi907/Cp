36dde9eb95443a1e0ca04f2c754dce1a2d0a8919
Aditya Sharma <76524120+adi907@users.noreply.github.com>
2022-07-17 07:03:43 -0700

    Create timeConversion.py

---
+++ Python/timeConversion.py
@@ -0,0 +1,9 @@
+def timeConversion(s):
+    if s[0:2]=="12" and s[-2:]=="AM":
+        return "00"+s[2:8]
+    elif s[-2:]=="AM":
+        return s[0:-2]
+    elif s[-2:]=='PM'and s[:2]=="12":
+        return s[:8]
+    else:
+        return str(12 + int(s[:2]))+ s[2:8]
