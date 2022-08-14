ccc02e5a72b0479199f21330608b71b377d62272
Aditya Sharma <76524120+adi907@users.noreply.github.com>
2022-08-14 04:49:10 -0700

    Day2: Arrays-II

--- Striver_SDEsheet/duplicate_inArray.cpp
+++ Striver_SDEsheet/duplicate_inArray.cpp
@@ -2,7 +2,7 @@
 // Time complexity:O(NlogN+N) ; Space complexity: O(1)
 
 // Better approach:Use boolean frequency array. As iterating through array;for each arr[i], mark the freq[arr[i]] with true. If any element already true; return it
-// Time complexity:O(N) ; Space complexity:O(1)
+// Time complexity:O(N) ; Space complexity:O(N)
 
 // Optimised approach{but modifies the array}: Use Linked list cycle method(not actual linked list). Start from index=0, keep moving where value of index is.arr[0]-> arr[arr[0]]->arr[arr[0]]-> ... Keep marking travelled positions with -1. And when you reach a place which already has -1.Stop and return the subsequent value which led you to it
 // Time complexity:O(N) ; Space complexity:O(1)
@@ -45,4 +45,3 @@ int findDuplicate(vector<int>& nums) {
         
     return turtle;
 }
-
